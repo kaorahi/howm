3e16f70440a779c79d064530b708eed67ffc7fdb
HIRAOKA Kazuyuki <khi@users.osdn.me>
2017-12-27 09:53:45 +0900

    move contents of howm-version.el to howm.el for simplicity

--- howm-version.el
+++
@@ -1,8 +0,0 @@
-;;; howm-version.el --- Wiki-like keyword jump between files
-;;; -*- Emacs-Lisp -*-
-;;; howm-version.el.  Generated from howm-version.el.in by configure.
-
-(provide 'howm-version)
-(defconst howm-version "1.4.5-snapshot")
-
-;;; howm-version.el ends here
